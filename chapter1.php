bd656c197889818e10bef5a04f1164ca1fa0a1b4
tianwangchong <q408596288@vip.qq.com>
2019-04-11 11:06:00 +0800

    快速开始

---
+++ chapter1.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tianwangchong
+ * Date: 2019/4/10
+ * Time: 14:54
+ */
+
+require 'vendor/autoload.php';
+
+use Elasticsearch\ClientBuilder;
+
+$client = ClientBuilder::create()->build();
+
+// // 创建一个索引
+// $params = [
+//     'index' => 'my_index',
+//     'body' => [
+//         'settings' => [
+//             'number_of_shards' => 2,
+//             'number_of_replicas' => 0
+//         ]
+//     ]
+// ];
+// $response = $client->indices()->create($params);
+// print_r($response);
+
+// // 删除一个索引
+// $deleteParams = [
+//     'index' => 'my_index'
+// ];
+// $response = $client->indices()->delete($deleteParams);
+// print_r($response);
+
+// // 删除一个文档
+// $params = [
+//     'index' => 'my_index',
+//     'type' => 'my_type',
+//     'id' => 'my_id'
+// ];
+// $response = $client->delete($params);
+// print_r($response);
+
+// // 搜索一个文档
+// $params = [
+//     'index' => 'my_index',
+//     'type' => 'my_type',
+//     'body' => [
+//         'query' => [
+//             'match' => [
+//                 'testField' => 'abc'
+//             ]
+//         ]
+//     ]
+// ];
+// $response = $client->search($params);
+// print_r($response);
+
+// // 获取一个文档
+// $params = [
+//     'index' => 'my_index',
+//     'type' => 'my_type',
+//     'id' => 'my_id'
+// ];
+// $response = $client->get($params);
+// print_r($response);
+
+// // 索引一个文档[创建]
+// $params = [
+//     'index' => 'my_index',
+//     'type' => 'my_type',
+//     'id' => 'my_id',
+//     'body' => ['testField' => 'abc']
+// ];
+// $response = $client->index($params);
+// print_r($response);
